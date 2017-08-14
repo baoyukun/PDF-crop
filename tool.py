e3a0b739f344f056bb7c932cf28e60fef8f13478
BAO Yukun <baoyukun1994@126.com>
2017-08-14 19:20:28 +0200

    Add comments for code
    Add comments for code

--- tool.py
+++ tool.py
@@ -12,11 +12,11 @@ def op(inputFilePath):
 
     for page in [inputStream.getPage(i) for i in range(inputStream.getNumPages())]:
         pageLeft = page
-		#注意：简单的assignment会导致左、右切割操作同一页面对象
+	#注意：简单的assignment会导致左、右切割操作同一页面对象
         pageRight = copy(page)
-		#切割得到左页面时，切割线比中线稍微向右去一点
+	#切割得到左页面时，切割线比中线稍微向右去一点
         pageLeft.mediaBox.upperRight = (pageLeft.mediaBox.getUpperRight_x(), pageLeft.mediaBox.getUpperRight_y() * 52/100)
-		#切割得到右页面时，切割线比中线稍微向左去一点
+	#切割得到右页面时，切割线比中线稍微向左去一点
         pageRight.mediaBox.lowerRight = (pageRight.mediaBox.getLowerRight_x(), pageRight.mediaBox.getUpperRight_y() * 48/100)
         outputStream.addPage(pageLeft)
         outputStream.addPage(pageRight)
